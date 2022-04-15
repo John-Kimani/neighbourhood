192a05662f9c7d10804efa45f1671e9fbec30096
John-Kimani <kimwaweru100@gmail.com>
2022-04-15 12:28:56 +0300

    Dev: Creates users app templates

--- users/views.py
+++ users/views.py
@@ -1,1 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+def register(request):
+    '''
+    New user registration
+    '''
+    form = UserCreationForm()
+    return render(request, 'users/register.html', {"form":form})
