44e82efec3fbaf31859bbfbb4b53b18f1940e461
John-Kimani <kimwaweru100@gmail.com>
2022-04-15 13:06:47 +0300

    Dev: Create register form

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
-import neighbours
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('neighbours.urls'))
+    path('', include('neighbours.urls')),
+
+    #users application
+    path('register/', user_views.register, name='register'),
 ]
