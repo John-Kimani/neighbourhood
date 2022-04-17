39cec103b3b84064ca5c313e721a6f94bf70a0a5
John-Kimani <kimwaweru100@gmail.com>
2022-04-18 01:18:53 +0300

    Dev: Creates userprofile templates

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
-from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils import timezone
 
 
 class Profile(models.Model):
@@ -12,7 +12,7 @@ class Profile(models.Model):
     profile_picture = CloudinaryField('images/', default='https://res.cloudinary.com/dbgbail9r/image/upload/v1649544556/profile_image_kfrlhw.png')
     bio = models.TextField(max_length=50, blank=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
-
+    date_joined = models.DateField(default=timezone.now)
 
     def __str__(self):
         return self.user.username
