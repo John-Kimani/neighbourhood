e70f3dfa838c3ab3012c6d1d6e367c9612984dfd
John-Kimani <kimwaweru100@gmail.com>
2022-04-17 23:40:05 +0300

    Validated user creation form

--- users/views.py
+++ users/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def register(request):
     '''
     New user registration
     '''
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('homepage')
+    else:
+        form = UserCreationForm()
     return render(request, 'users/register.html', {"form":form})
