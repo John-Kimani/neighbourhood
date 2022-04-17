8553bd4c25075083f08ce925628b1f818faac69c
John-Kimani <kimwaweru100@gmail.com>
2022-04-18 02:06:10 +0300

    Fix:Profile update

--- users/views.py
+++ users/views.py
@@ -30,8 +30,17 @@ def update_user_profile(request):
     '''
     User profile view function
     '''
-    user_form = UserUpdateForm()
-    profile_form = ProfileUpdateForm()
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES ,instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your profile information has been updated')
+            return redirect('profile')
+    else :
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
     subject = {
         'user_form': user_form,
         'profile_form': profile_form,
