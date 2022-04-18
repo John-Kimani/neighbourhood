9fa397e447c70c44d5a28707b7e3e783c95e71ef
John-Kimani <kimwaweru100@gmail.com>
2022-04-18 20:44:29 +0300

    Dev: Create neighborhood class

--- neighbours/models.py
+++ neighbours/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Neighborhood(models.Model):
+    '''
+    Neighborhood class
+    '''
+    name = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
+    description = models.TextField()
+    hood_img = CloudinaryField('images/')
+
+    def __str__(self):
+        return self.name
+
+    def save_neighborhood(self):
+        self.save()
+    
+    def delete_neighborhood(self):
+        self.delete()
