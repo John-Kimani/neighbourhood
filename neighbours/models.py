3b7f689c59455edee8cda36c53eb1f4e42ae6516
John-Kimani <kimwaweru100@gmail.com>
2022-04-18 21:33:48 +0300

    Dev: Restructrure business model

--- neighbours/models.py
+++ neighbours/models.py
@@ -31,6 +31,7 @@ class Business(models.Model):
     contact = models.EmailField()
     neighborhood = models.ForeignKey(Neighborhood, null=True, blank=True, on_delete=models.CASCADE)
     description = models.TextField()
+    business_img = CloudinaryField('images/')
 
     def __str__(self):
         return self.name
@@ -51,6 +52,7 @@ class Post(models.Model):
     hood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE, related_name='posts')
     message = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    hashtags = models.CharField(max_length=60, null=False, blank=True, default='#')
 
     def __str__(self):
         return self.title
