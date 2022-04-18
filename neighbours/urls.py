91fa42d52f29020cf5bbeb9fb2a7c169632a0b27
John-Kimani <kimwaweru100@gmail.com>
2022-04-18 19:37:40 +0300

    Dev: Creates additional templates

--- neighbours/urls.py
+++ neighbours/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('hood/', views.neighboorhood, name='hoodpage'),
+    path('business/', views.business, name='businesspage'),
 ]
--- neighbours/views.py
+++ neighbours/views.py
@@ -5,3 +5,16 @@ def homepage(request):
     Homepage view function
     '''
     return render(request, 'neighbours/homepage.html')
+
+
+def neighboorhood(request):
+    '''
+    Nighbourhood view function
+    '''
+    return render(request, 'neighbours/hood.html')
+
+def business(request):
+    '''
+    Business view function
+    '''
+    return render(request, 'neighbours/business.html')
